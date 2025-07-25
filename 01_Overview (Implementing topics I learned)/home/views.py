9741986572eb9f35b52d0797ecd9d34599b5620f
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-07-25 09:11:25 +0000

    models, request, pages, form, endpoint

--- 01_Overview (Implementing topics I learned)/home/views.py
+++ 01_Overview (Implementing topics I learned)/home/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from datetime import datetime
+from home.models import Contact
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
 
 
 def index(request):
     context = {
         'variable': 'this is sent'
     }
+    messages.success(request, "this is test messages")
     return render(request, 'index.html', context)
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+@csrf_exempt
+def contact(request):
+
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+
+        contact = Contact(name=name, email=email, phone=phone,
+                          desc=desc, date=datetime.today())
+        contact.save()
+        print(contact)
+        messages.success(request, 'Profile Created')
+
+    return render(request, 'contact.html')
